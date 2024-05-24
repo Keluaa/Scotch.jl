b6cc189579c65fde407311c25eef09d88f6eb7cd
Documenter.jl <documenter@juliadocs.github.io>
2024-05-24 14:17:57 +0000

    build based on 5eb88fe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Scotch","category":"page"},{"location":"#Scotch","page":"Home","title":"Scotch","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Scotch.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Scotch]","category":"page"}]
+}
